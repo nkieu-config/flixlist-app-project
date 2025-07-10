7ba55e8cc9ed29bd7cb93d22c3b84ee93aaecbfb
nkieu-config <natthachak.config@gmail.com>
2025-07-10 23:06:49 +0700

    feat: Add API fetching, movie details, and watch list management

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,10 @@
-import { StrictMode, useState } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-// import StarRating from "./StarRating.jsx";
-
-// export default function Test() {
-//   const [movieRating, setMovieRating] = useState(0);
-
-//   return (
-//     <div>
-//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-//       <p>This movie was rated {movieRating} stars</p>
-//     </div>
-//   );
-// }
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    {/* <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    /> */}
-    {/* <StarRating size={24} color="red" className="test" defaultRating={2} />  */}
-    {/* <Test /> */}
   </StrictMode>
 );
